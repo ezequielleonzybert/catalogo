d1a89ac18533cf3c9c1abf89c005852c25b5a5a9
Ezequiel <ezequielleonzybert@gmail.com>
2025-02-25 18:40:38 -0300

    update

--- docs/app.js
+++ docs/app.js
@@ -1,46 +1,41 @@
 const productCatalog = document.getElementById('products');
 const categorias = document.getElementsByClassName('categorias')[0];
 const arrow = document.getElementById('arrow');
+let productDataArray = [];
 
-const isGitHubPages = location.hostname === 'ezequielleonzybert.github.io';
-const baseImageUrl = isGitHubPages
-    ? 'https://raw.githubusercontent.com/ezequielleonzybert/catalogo/refs/heads/main/docs/images/'
-    : 'images/';
-
-fetch('data/products.csv')
+fetch('./data/products.csv')
     .then(response => response.text())
     .then(data => {
-        const parsedProducts = parseCSV(data);
-        renderProducts(parsedProducts);
+        productDataArray = parseCSV(data);
+        renderProducts(productDataArray);
     })
     .catch(error => console.error('Error fetching the CSV file:', error));
 
 function parseCSV(data) {
     const rows = data.split('\n').slice(1); // Omitir la cabecera
-    return rows.map(row => {
-        const [name, price, description, category, tags] = row.split(',');
-        return {
-            name,
-            price,
-            description,
-            category: category ? category.trim() : '',
-            tags: tags ? tags.toLowerCase().trim() : ''
-        };
-    });
+
+    return rows
+        .filter(row => row.trim() !== "") // Ignorar líneas vacías
+        .map(row => {
+            // Expresión regular para dividir en comas, ignorando las dentro de comillas
+            const regex = /(".*?"|[^,]+)(?=\s*,|\s*$)/g;
+            const matches = [...row.matchAll(regex)].map(m => m[0]);
+
+            // Eliminar comillas externas si hay
+            const cleanValues = matches.map(value => value.replace(/^"|"$/g, ''));
+
+            const [name, price, description, category, tags] = cleanValues;
+
+            return {
+                name,
+                price,
+                description,
+                category: category ? category.trim() : '',
+                tags: tags ? tags.toLowerCase().trim() : ''
+            };
+        });
 }
 
-function formatProductName(name) {
-    return name.toLowerCase().replace(/ /g, '_');
-}
-
-async function loadImage(url) {
-    return new Promise((resolve, reject) => {
-        const img = new Image();
-        img.onload = () => resolve(url);
-        img.onerror = () => reject(url);
-        img.src = url;
-    });
-}
 
 async function renderProducts(parsedProducts) {
     productCatalog.innerHTML = '';
@@ -50,11 +45,10 @@ async function renderProducts(parsedProducts) {
         productElement.setAttribute('data-category', product.category);
         productElement.setAttribute('data-tags', product.tags);
 
-        const formattedName = formatProductName(product.name);
-        const imageUrl = `${baseImageUrl}${formattedName}/0.webp`;
+        const formattedName = cleanText(product.name);
 
         const img = document.createElement('img');
-        img.src = imageUrl;
+        img.src = `./images/${formattedName}/0.webp`
         img.alt = product.name;
 
         productElement.appendChild(img);
@@ -62,12 +56,19 @@ async function renderProducts(parsedProducts) {
         const formattedPrice = Number(product.price).toLocaleString('es-AR');
         productElement.innerHTML += `
             <h2>${product.name}</h2>
-            <p class="price">$${formattedPrice}</p>
         `;
 
         productCatalog.appendChild(productElement);
     }
+
+    // Una vez que los productos están en el DOM, verificamos la URL
+    const params = new URLSearchParams(window.location.search);
+    const productName = params.get('producto');
+    if (productName) {
+        openModalFromURL(productName);
+    }
 }
+
 
 function filterAndSearchProducts() {
     const query = searchbox.value.toLowerCase().trim();
@@ -102,7 +103,7 @@ document.querySelectorAll('.categoria input[type="checkbox"]').forEach(checkbox 
 let isHidden = true;
 
 function toggleCategorias() {
-    categorias.classList.toggle('disappear');
+    categorias.classList.toggle('appear');
     arrow.classList.toggle('rotate180');
     isHidden = !isHidden;
 
@@ -130,3 +131,295 @@ window.addEventListener('resize', () => {
     }
     prev_width = window.innerWidth;
 });
+
+//MODAL POPUP
+
+const modal = document.querySelector('.modal');
+const modalContent = document.querySelector('.modal-content');
+const back = document.querySelector('#back');
+let scrollY;
+
+productCatalog.addEventListener('click', (event) => {
+    const productElement = event.target.closest('.product');
+    if (!productElement) return;
+
+    const productName = productElement.querySelector('h2').textContent;
+    const productData = productDataArray.find(p => cleanText(p.name) === cleanText(productName));
+
+    if (productData) {
+        openModal(productData);
+    }
+});
+
+async function loadImage(url) {
+    try {
+        const response = await fetch(url, { method: 'HEAD' });
+        if (!response.ok) return null;
+        return url;
+    } catch {
+        return null;
+    }
+}
+
+async function loadGallery(formattedProductName) {
+    const modalContent = document.querySelector('.modal-content');
+
+    const loader = document.createElement('div');
+    loader.classList.add('loader');
+    modalContent.appendChild(loader);
+
+    const galleryContainer = document.createElement('div');
+    galleryContainer.classList.add('scroll-gallery');
+
+    let index = 1;
+    let images = [];
+
+    while (true) {
+        const imageUrl = `./images/${formattedProductName}/${index}.webp`;
+        const loadedImage = await loadImage(imageUrl);
+
+        if (!loadedImage) break; // Si no se encuentra, termina el bucle
+        const img = document.createElement("img");
+        img.src = loadedImage;
+        img.alt = `${formattedProductName} ${index}`;
+        galleryContainer.appendChild(img);
+        images.push(img);
+        index++;
+    }
+
+    loader.remove();
+
+    // Crear el contenedor de puntos
+    const dotsContainer = document.createElement('div');
+    dotsContainer.classList.add('dots-container');
+
+    // Crear los puntos según la cantidad de imágenes
+    images.forEach((_, i) => {
+        const dot = document.createElement('span');
+        dot.classList.add('dot');
+        if (i === 0) dot.classList.add('active'); // Marcar el primer punto como activo
+        dotsContainer.appendChild(dot);
+    });
+
+    // Insertar la galería y los puntos en el modal
+    modalContent.prepend(galleryContainer);
+    modalContent.appendChild(dotsContainer);
+
+    // Actualizar los puntos al desplazarse
+    galleryContainer.addEventListener('scroll', () => {
+        const scrollPosition = galleryContainer.scrollLeft;
+        const imageWidth = galleryContainer.clientWidth;
+        const activeIndex = Math.round(scrollPosition / imageWidth);
+
+        // Actualizar los puntos
+        const dots = dotsContainer.querySelectorAll('.dot');
+        dots.forEach((dot, i) => {
+            if (i === activeIndex) {
+                dot.classList.add('active');
+            } else {
+                dot.classList.remove('active');
+            }
+        });
+    });
+
+    // Permitir hacer clic en los puntos para desplazarse
+    dotsContainer.querySelectorAll('.dot').forEach((dot, i) => {
+        dot.addEventListener('click', () => {
+            const imageWidth = galleryContainer.clientWidth;
+            galleryContainer.scrollTo({
+                left: i * imageWidth,
+                behavior: 'smooth', // Desplazamiento suave
+            });
+        });
+    });
+
+    //ARROW-SIDE
+    fetch("./images/icons/arrow-side.svg")
+        .then(res => res.text())
+        .then(data => {
+            let arrowRight = new DOMParser().parseFromString(data, "image/svg+xml").querySelector("svg");
+            let arrowLeft = new DOMParser().parseFromString(data, "image/svg+xml").querySelector("svg");
+            arrowRight.classList.add("arrow-side");
+            arrowLeft.classList.add("arrow-side", "arrow-left")
+            document.querySelector(".modal-content").appendChild(arrowRight);
+            document.querySelector(".modal-content").appendChild(arrowLeft);
+
+            // Agregar event listener después de insertar las flechas
+            modalContent.addEventListener("click", (event) => {
+                const scrollAmount = galleryContainer.clientWidth; // Se mueve por el ancho de la galería
+
+                if (event.target.closest(".arrow-side")) {
+                    if (event.target.closest(".arrow-left")) {
+                        galleryContainer.scrollBy({ left: -scrollAmount });
+                    } else {
+                        galleryContainer.scrollBy({ left: scrollAmount });
+                    }
+                }
+            });
+        })
+        .catch(err => console.error("Error:", err));
+
+}
+
+function openModal(productData) {
+    if (!productData) return;
+
+    const modalContent = document.querySelector('.modal-content');
+
+    loadGallery(cleanText(productData.name));
+
+    const formattedPrice = Number(productData.price).toLocaleString('es-AR');
+    modalContent.innerHTML = `
+        <div class="product-info">
+            <h2>${productData.name}</h2>
+            <p class="price">$${formattedPrice}</p>
+            <p class="description">${productData.description}</p>
+        </div>
+    `;
+
+    // Mostrar el modal
+    scrollY = window.scrollY;
+    document.body.style.top = `-${scrollY}px`;
+    document.body.style.paddingRight = `${window.innerWidth - document.body.clientWidth}px`;
+    back.style.top = "0";
+    document.body.classList.add('no-scroll');
+
+    modal.classList.remove('hidden');
+    requestAnimationFrame(() => {
+        modal.classList.add('appear');
+    });
+
+    // Guardar en la URL
+    history.pushState({ modalOpen: true }, '', `?producto=${cleanText(productData.name)}`);
+}
+
+function openModalFromURL(productName) {
+    const productData = productDataArray.find(p => cleanText(p.name) === cleanText(productName));
+
+    if (productData) {
+        openModal(productData);
+    }
+}
+
+function closeModal() {
+    modal.classList.remove('appear');
+    document.body.style.paddingRight = `0`;
+    document.body.classList.remove('no-scroll');
+
+
+    modal.addEventListener('transitionend', () => {
+        modal.classList.add('hidden');
+
+    }, { once: true });
+
+    window.scrollTo(0, scrollY);
+
+    if (history.state?.modalOpen) {
+        history.replaceState(null, '', window.location.pathname); // Elimina el parámetro sin afectar el historial
+    }
+}
+
+modal.addEventListener('click', (event) => {
+    if (!modalContent.contains(event.target)) {
+        closeModal();
+    }
+});
+
+window.addEventListener("popstate", (event) => {
+    const params = new URLSearchParams(window.location.search);
+    const productName = params.get('producto');
+
+    if (productName) {
+        openModalFromURL(productName); // Reabre el modal si hay un producto en la URL
+    } else {
+        closeModal();
+    }
+});
+
+function cleanText(texto) {
+    return texto
+        .normalize("NFD") // Descompone caracteres con tildes
+        .replace(/[\u0300-\u036f]/g, "") // Elimina las tildes
+        .replace(/\s+/g, "_") // Elimina los espacios
+        .toLowerCase();
+}
+
+// function enableDragScroll(galleryContainer) {
+//     let isDragging = false;
+//     let startX, scrollLeft;
+
+//     galleryContainer.addEventListener('mousedown', (e) => {
+//         isDragging = true;
+//         startX = e.pageX;
+//         scrollLeft = galleryContainer.scrollLeft;
+//         galleryContainer.style.cursor = 'grabbing';
+//         e.preventDefault();
+//     });
+
+//     galleryContainer.addEventListener('mouseleave', () => {
+//         if (isDragging) {
+//             isDragging = false;
+//             galleryContainer.style.cursor = 'grab';
+//             snapToNearestImage(galleryContainer)
+//         }
+//     });
+
+//     galleryContainer.addEventListener('mouseup', () => {
+//         if (isDragging) {
+//             isDragging = false;
+//             galleryContainer.style.cursor = 'grab';
+//             snapToNearestImage(galleryContainer)
+//         }
+//     });
+
+//     galleryContainer.addEventListener('mousemove', (e) => {
+//         if (!isDragging) return;
+//         e.preventDefault();
+//         const x = e.pageX;
+//         const walk = (x - startX) * 1.5;
+//         galleryContainer.scrollLeft = scrollLeft - walk;
+//     });
+// }
+
+// function snapToNearestImage(galleryContainer) {
+//     const images = galleryContainer.querySelectorAll('img');
+//     const galleryScrollLeft = galleryContainer.scrollLeft;
+//     const galleryWidth = galleryContainer.clientWidth;
+
+//     let closestImage = null;
+//     let closestDistance = Infinity;
+
+//     // Encuentra la imagen más cercana al scroll actual
+//     images.forEach((img, index) => {
+//         const imgLeft = index * galleryWidth; // Posición de la imagen
+//         const distance = Math.abs(galleryScrollLeft - imgLeft);
+
+//         if (distance < closestDistance) {
+//             closestDistance = distance;
+//             closestImage = img;
+//         }
+//     });
+
+//     // Ajusta el scroll a la imagen más cercana
+//     if (closestImage) {
+//         const targetScrollLeft = Array.from(images).indexOf(closestImage) * galleryWidth;
+//         galleryContainer.scrollTo({
+//             left: targetScrollLeft,
+//             behavior: 'smooth', // Desplazamiento suave
+//         });
+//     }
+// }
+
+//LOADER ANIMATION
+
+// window.addEventListener('load', () => {
+//     setTimeout(() => {
+//         const pageLoader = document.getElementById('page-loader');
+
+//         // Oculta el loader
+//         pageLoader.style.display = 'none';
+
+//         // Muestra el contenido
+//         document.body.classList.remove('hidden');
+//     }, 1500); // 2000 milisegundos = 2 segundos
+// });
